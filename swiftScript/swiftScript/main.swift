089c30f78339b1326418a760061d749eda70fe4e
haron1020 <haron1020@yandex.ru>
2015-10-06 15:43:31 +0300

    Initial commit

---
+++ swiftScript/swiftScript/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftScript
+//
+//  Created by Andrey Nazarov on 06.10.15.
+//  Copyright © 2015 Andrey Nazarov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

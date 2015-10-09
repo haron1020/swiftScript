db476392897a75e694ba4868159cd8e9041369af
haron1020 <haron1020@yandex.ru>
2015-10-09 21:28:49 +0300

    Работает удаление всех пробелов, идет работа над архитектурой. Работает для 2х операндов + и -

--- swiftScript/swiftScript/main.swift
+++ swiftScript/swiftScript/main.swift
@@ -9,33 +9,54 @@
 
 import Foundation
 
-dump(Process.arguments)
-
-enum Types {
-    case binaryOperation(op: String, fn: (Double, Double) -> Double)
-    case argument(op: String)
-    case unaryOperation(op: String, fn:(Double) -> Double)
-    case const(Op: String -> Double)
-    case function(op: String, fn: (Double) -> Double)
+extension String {
+    subscript(i: Int) -> String {
+        return String(self[self.startIndex.advancedBy(i)])
+    }
 }
 
+func eraseSpaces(inout arg: String) {
+    for (var i = 0; i<arg.characters.count;i++) {
+        if arg[i] == " " {
+            arg.removeAtIndex(arg.startIndex.advancedBy(i))
+            --i
+        }
+    }
+}
+
+enum Types {
+    case binaryOperation
+    case argument
+    case unaryOperation(op: String, fn:(Double) -> Double)
+    case const(Op: String -> Double)
+    //case function(op: String, fn: (Double) -> Double)
+}
 class Node {
-    var key:String?
+    var key:String
+    var kind:Types?
     var left:Node?
     var right:Node?
     var parent:Node?
     init() {
         self.left = nil
         self.right = nil
-        self.key = nil
+        self.key = ""
         self.parent = nil
+        kind = nil
     }
 }
-
+/*
+MODES:
+0 - need parent
+1 - need left
+2 - need right
+*/
 class Tree {
     var root: Node?
+    var mode: Int
     init() {
         root = nil
+        mode = 0
     }
     func isEmpty() -> Bool {
         if self.root == nil {
@@ -43,16 +64,73 @@ class Tree {
         }
         return false
     }
+    func checkNodes() {
+        if root == nil {
+            root = Node()
+            mode++
+        }
+        switch mode {
+        case 0:
+            break
+        case 1:
+            root?.left = Node()
+        case 2:
+            root?.right = Node()
+        default:
+            break
+        }
+        mode = (mode+1)%3
+    }
     
+    func getToken(input: String) {
+        self.checkNodes()
+        for s in input.characters {
+            switch s {
+            case "0","1","2","3","4","5","6","7","8","9":
+                if mode == 0 {
+                    root!.right!.key.append(s)
+                    root!.right!.kind = Types.argument
+                    root!.right!.parent = root
+                }
+                if mode == 2 {
+                    root!.left!.key.append(s)
+                    root!.left!.kind = Types.argument
+                    root!.left!.parent = root
+                }
+                break
+            case "+":
+                root!.key = "+"
+                self.checkNodes()
+                root?.kind = Types.binaryOperation
+            case "-":
+                root!.key = "-"
+                self.checkNodes()
+                root?.kind = Types.binaryOperation
+            default:
+                break
+            }
+        }
+    }
+    func calc() {
+        switch root!.key {
+        case "+":
+            root!.key = String(Double((root!.left!.key))! + Double(root!.right!.key)!)
+        case "-":
+            root!.key = String(Double((root!.left!.key))! - Double(root!.right!.key)!)
+        default:
+            break
+        }
+        root!.kind = Types.argument
+    }
 }
 
-var myTree = Tree()
-print(myTree.isEmpty())
-let input = Process.arguments[1]
+//dump(Process.arguments)
+var input = Process.arguments[1]
+//var input = "    3         - 6        9"
+eraseSpaces(&input)
 print("Input is: \(input)" )
-/*
-    testing
-        git
-            testing
-                git
-*/
+
+var myTree = Tree()
+myTree.getToken(input)
+myTree.calc()
+print(myTree.root?.key)

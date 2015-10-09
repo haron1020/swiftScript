9e1e1e0b134781bed9f8328693b5f57584550c97
haron1020 <haron1020@yandex.ru>
2015-10-09 15:08:51 +0300

    Первые наработки для создания архитектуры списка

--- swiftScript/swiftScript/main.swift
+++ swiftScript/swiftScript/main.swift
@@ -10,6 +10,47 @@
 import Foundation
 
 dump(Process.arguments)
+var input:String = Process.arguments[1]
+input.removeAtIndex(input.characters.indexOf(" ")!)
+dump(input)
 
-let input = Process.arguments[1]
+enum Types {
+    case binaryOperation(op: String, fn: (Double, Double) -> Double)
+    case argument(op: String)
+    case unaryOperation(op: String, fn:(Double) -> Double)
+    case const(Op: String -> Double)
+    case function(op: String, fn: (Double) -> Double)
+}
+
+class Node {
+    var key:String?
+    var left:Node?
+    var right:Node?
+    var parent:Node?
+    init() {
+        self.left = nil
+        self.right = nil
+        self.key = nil
+        self.parent = nil
+    }
+}
+
+class Tree {
+    var root: Node?
+    init() {
+        root = nil
+    }
+    func isEmpty() -> Bool {
+        if self.root == nil {
+            return true
+        }
+        return false
+    }
+  
+}
+
+var myTree = Tree()
+print(myTree.isEmpty())
+//myTree.addNode(input)
+
 print("Input is: \(input)" )
